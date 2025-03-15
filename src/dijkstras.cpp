01c0885e638e4e2435ac9ffe97a603f3b48a6aa5
chanwt86 <steve.wt.chan@gmail.com>
2025-03-15 23:03:44 +0000

    first commit(ladder)

---
+++ src/dijkstras.cpp
@@ -0,0 +1,45 @@
+#include "dijkstras.h"
+#include <queue>
+
+vector<int> dijkstra_shortest_path(const Graph& G, int source, vector<int>& previous){
+    int n = G.numVertices;
+
+    vector<int> distance(n, INF);
+    previous.assign(n, -1);
+    vector<bool> visited(n, false);
+
+    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+
+    distance[source] = 0;
+    pq.push({0, source});
+
+    while (!pq.empty()) {
+        int u = pq.top().second;
+        pq.pop();
+
+        if (visited[u]) {
+            continue;
+        }
+        visited[u] = true;
+
+        for (const Edge &edge : G.adjacencyList[u]) {
+            int v = edge.dst;
+            int weight = edge.weight;
+
+            if (!visited[v] && distance[u] + weight < distance[v]) {
+                distance[v] = distance[u] + weight;
+                previous[v] = u;
+                pq.push({distance[v], v});
+            }
+        }
+    }
+    return distance;
+}
+
+vector<int> extract_shortest_path(const vector<int>& /*distances*/, const vector<int>& previous, int destination){
+
+}
+
+void print_path(const vector<int>& v, int total){
+
+}
---
+++ src/ladder.cpp
@@ -0,0 +1,102 @@
+#include "ladder.h"
+#include <cstddef>
+#include <fstream>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+void error(string word1, string word2, string msg){
+    cout << word1 << " and " << word2 << " ERROR: " << msg << endl;
+}
+
+bool edit_distance_within(const std::string& str1, const std::string& str2, int d){
+    int len1 = str1.size();
+    int len2 = str2.size();
+
+    if (abs(len1 - len2) > d) {
+        return false;
+    }
+
+    int i = 0, j = 0, diff = 0;
+
+    while (i < len1 && j < len2) {
+        if (str1[i] != str2[j]) {
+            diff++;
+            if (diff > d) {
+                return false;
+            }
+            if (len1 > len2) {
+                i++;
+            } else if (len1 < len2){
+                j++;
+            } else {
+                i++;
+                j++;
+            }
+        }
+    }
+
+    diff += (len1 - i) + (len2 - j);
+
+    return diff <= d;
+}
+
+bool is_adjacent(const string& word1, const string& word2){
+    return edit_distance_within(word1, word2, 1);
+}
+
+vector<string> generate_word_ladder(const string& begin_word, const string& end_word, const set<string>& word_list){
+    queue<vector<string>> ladder_queue;
+    ladder_queue.push({begin_word});
+
+    unordered_set<string> visited;
+    visited.insert(begin_word);
+
+    while (!ladder_queue.empty()){
+        vector<string> ladder = ladder_queue.front();
+        ladder_queue.pop();
+        string last_word = ladder.back();
+
+        for (const string& word : word_list) {
+            if (is_adjacent(last_word, word) && !visited.count(word)) {
+                visited.insert(word);
+                vector<string> new_ladder = ladder;
+                new_ladder.push_back(word);
+                if (word == end_word) {
+                    return new_ladder;
+                }
+                ladder_queue.push(new_ladder);
+            }
+        }
+    }
+    return {};
+}
+
+void load_words(set<string> & word_list, const string& file_name){
+    ifstream file(file_name);
+    string word;
+    while (file >> word) {
+        word_list.insert(word);
+    }
+    file.close();
+}
+
+void print_word_ladder(const vector<string>& ladder){
+    for (size_t i = 0; i < ladder.size(); ) {
+        cout << ladder[i];
+        if (i != ladder.size() - 1) {
+            cout << "->";
+        }
+    }
+}
+
+void verify_word_ladder(){
+    set<string> word_list;
+    load_words(word_list, "words.txt");
+    my_assert(generate_word_ladder("cat", "dog", word_list).size() == 4);
+    my_assert(generate_word_ladder("marty", "curls", word_list).size() == 6);
+    my_assert(generate_word_ladder("code", "data", word_list).size() == 6);
+    my_assert(generate_word_ladder("work", "play", word_list).size() == 6);
+    my_assert(generate_word_ladder("sleep", "awake", word_list).size() == 8);
+    my_assert(generate_word_ladder("car", "cheat", word_list).size() == 4);
+}
