803d7fd97775f489f0720f7e70c0c0462f391e45
chanwt86 <steve.wt.chan@gmail.com>
2025-03-15 23:13:42 +0000

    test for ladder

--- src/dijkstras.cpp
+++ src/dijkstras.cpp
@@ -1,3 +1,4 @@
+/*
 #include "dijkstras.h"
 #include <queue>
 
@@ -36,10 +37,11 @@ vector<int> dijkstra_shortest_path(const Graph& G, int source, vector<int>& prev
     return distance;
 }
 
-vector<int> extract_shortest_path(const vector<int>& /*distances*/, const vector<int>& previous, int destination){
+vector<int> extract_shortest_path(const vector<int>& , const vector<int>& previous, int destination){
 
 }
 
 void print_path(const vector<int>& v, int total){
 
 }
+*/
