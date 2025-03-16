1ff5170d2c7a914e72d8ac69129ca02d583fe1b1
chanwt86 <steve.wt.chan@gmail.com>
2025-03-16 00:48:43 +0000

    implementations for dijkstras

--- src/dijkstras.cpp
+++ src/dijkstras.cpp
@@ -1,14 +1,16 @@
-/*
 #include "dijkstras.h"
+#include <algorithm>
+#include <iterator>
 #include <queue>
 
 vector<int> dijkstra_shortest_path(const Graph& G, int source, vector<int>& previous){
-    int n = G.size();
+    int n = G.numVertices;
     vector<int> distances(n, INF);
     vector<bool> visited(n, false);
-    distances[source] = 0
-    previous[source] = -1;
+    distances[source] = 0;
+    previous.assign(n, -1);
 
+    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
     pq.push({0, source});
 
     while (!pq.empty()) {
@@ -20,25 +22,41 @@ vector<int> dijkstra_shortest_path(const Graph& G, int source, vector<int>& prev
         }
         visited[u] = true;
 
-        for (const Edge &edge : G.adjacencyList[u]) {
+        for (const Edge & edge : G) {
             int v = edge.dst;
             int weight = edge.weight;
 
-            if (!visited[v] && distance[u] + weight < distance[v]) {
-                distance[v] = distance[u] + weight;
+            if (!visited[v] && distances[u] + weight < distances[v]) {
+                distances[v] = distances[u] + weight;
                 previous[v] = u;
-                pq.push({distance[v], v});
+                pq.push({distances[v], v});
             }
         }
     }
-    return distance;
+    return distances;
 }
 
-vector<int> extract_shortest_path(const vector<int>& , const vector<int>& previous, int destination){
+vector<int> extract_shortest_path(const vector<int>& distances, const vector<int>& previous, int destination){
+    vector<int> path;
 
+    if (distances[destination] == INF) {
+        return path;
+    }
+
+    for (int at = destination; at != -1; at = previous[at]) {
+        path.push_back(at);
+    }
+
+    reverse(path.begin(), path.end());
+    return path;
 }
 
 void print_path(const vector<int>& v, int total){
-
+    for (int i = 0; i < v.size(); ++i) {
+        cout << v[i];
+        if (i != v.size() - 1) {
+            cout << " ";
+        }
+    }
+    cout << endl << "Total cost is " << total << endl;
 }
-*/
